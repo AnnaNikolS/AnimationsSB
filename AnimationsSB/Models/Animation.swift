2570c0365e1f993b6759fd43210ae89e391e186f
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-03-19 22:43:27 +0300

    Creating groups, folders, adding a framework

---
+++ AnimationsSB/Models/Animation.swift
@@ -0,0 +1,8 @@
+//
+//  Animation.swift
+//  AnimationsSB
+//
+//  Created by Анна on 19.03.2024.
+//
+
+import Foundation
---
+++ AnimationsSB/Services/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  AnimationsSB
+//
+//  Created by Анна on 19.03.2024.
+//
+
+import Foundation
