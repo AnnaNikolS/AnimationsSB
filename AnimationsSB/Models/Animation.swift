64226d081e9fa866d37777080aee5e87d58600c4
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-03-20 01:14:19 +0300

    description of labels, button and springView

--- AnimationsSB/Models/Animation.swift
+++ AnimationsSB/Models/Animation.swift
@@ -20,8 +20,8 @@ struct Animation {
         Animation(
             preset: DataStore().animations.randomElement()?.rawValue ?? "pop",
             curve: DataStore().curves.randomElement()?.rawValue ?? "easeInBack",
-            force: Double.random(in: 0.5...3.0),
-            duration: Double.random(in: 0.5...3.0),
+            force: Double.random(in: 1.0...4.0),
+            duration: Double.random(in: 0.8...2.0),
             delay: 1.0
         )
     }  
--- AnimationsSB/ViewControllers/ViewController.swift
+++ AnimationsSB/ViewControllers/ViewController.swift
@@ -10,6 +10,7 @@ import SpringAnimation
 
 final class ViewController: UIViewController {
     
+    // MARK: - IB Outlets
     @IBOutlet weak var springView: SpringView!
     @IBOutlet weak var presetLabel: SpringLabel!
     @IBOutlet weak var curveLabel: SpringLabel!
@@ -17,19 +18,44 @@ final class ViewController: UIViewController {
     @IBOutlet weak var durationLabel: SpringLabel!
     @IBOutlet weak var delayLabel: SpringLabel!
     
-    let animation = Animation.getAnimation()
+    @IBOutlet weak var animationButton: SpringButton!
     
+    // MARK: - Private properties
+    private var animation = Animation.getAnimation()
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        animationButton.setTitle("Run \(animation.preset)", for: .normal)
+        description()
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func startAnimationButton(_ sender: UIButton) {
+        
+        description()
+        
+        springView.animation = animation.preset
+        springView.force = animation.force
+        springView.delay = animation.delay
+        springView.duration = animation.duration
+        springView.curve = animation.curve
+        springView.animate()
+        
+        animation = Animation.getAnimation()
+       
+        sender.setTitle("Run \(animation.preset)", for: .normal)
+    }
+}
+
+// MARK: - Extension View Controller
+extension ViewController {
+    func description() {
         presetLabel.text = "preset: \(animation.preset)"
         curveLabel.text = "curve: \(animation.curve)"
         forceLabel.text = "force: \(String(format: "%.02f", animation.force))"
         durationLabel.text = "duration: \(String(format: "%.02f", animation.duration))"
         delayLabel.text = "delay: \(String(format: "%.02f", animation.delay))"
     }
-
-    @IBAction func startAnimationButton(_ sender: SpringButton) {
-    }
-    
 }
 
